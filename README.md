08f4e5519642aad508165598d9cde2227c74adf6
Joe Ross <joeross999@gmail.com>
2017-11-16 18:33:07 -0500

    get location details

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ I have used code from various sources to accomplish this project.
 
 * Code from the Udacity Nano degree hosted on GitHub [here](https://github.com/udacity/ud864)
 * Examples from Google's API tutorials located [here](https://developers.google.com/maps/documentation/javascript/tutorial)
+*Examples from the jQuery API documentation located [here](http://api.jquery.com/)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var map;
 var markers = [];
 var infowindow;
+var placesService;
 function initMap() {
     /***************
      * Setup Map
@@ -9,7 +10,8 @@ function initMap() {
         center: {lat: -41.7647719, lng: 172.7009717},
         zoom: 7,
         styles: darkGray,
-        // mapTypeControl: false,
+        mapTypeControl: false,
+        disableDefaultUI: true,
         // gestureHandling: 'none',
         // zoomControl: false,
         // minZoom: 7,
@@ -22,19 +24,16 @@ function initMap() {
     var request = {
         query: 'Nationl parks in new zealand'
     };
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+    placesService = new google.maps.places.PlacesService(map);
+    console.log()
+    placesService.textSearch(request, callback);
     
     function callback(results, status) {
-        console.log(results);
-        console.log(status);
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 markers.push(createMarker(results[i]));
             }
         }
-        console.log("callback");
-        console.log(markers);
     }
     
     infowindow = new google.maps.InfoWindow();
@@ -53,27 +52,68 @@ function createMarker(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
-        position: place.geometry.location
+        position: place.geometry.location,
+        id: place.place_id
     });
-    
+    // marker.addEventListener('click', populateInfoWindow);
     google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
+        // infowindow.setContent(place.name);
+        // infowindow.open(map, this);
+        populateInfoWindow(marker, infowindow);
+        console.log("addListener")
     });
+    console.log('createMarker')
 }
 
 function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
+        console.log("populateInfoWindow")
+        details = getLocationDetails(marker, function(details, status){
+            console.log(details);
+            if (status === google.maps.places.PlacesServiceStatus.OK){
+            infowindow.setContent(parkHTML(details));
+            }else{
+                console.log('there is a problem');
+            }
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
         });
     }
 }
+
+function parkHTML(details) {
+    var html = '<div><h2>' + details.name + '</h2><p>' + details.formatted_address + '<br>Rating: ' + details.rating + '<br><a href="' + details.url + '">View Park</a></p></div>'
+    console.log('html');
+    console.log(html);
+    return html;
+}
+
+function getLocationDetails(marker, callback){
+    console.log(marker);
+    placesService.getDetails({
+        placeId: marker.id
+      }, function(place, status) {
+            callback(place, status);
+      });
+}
+
+// function getWeatherForLocation(location) {
+//     request = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=' + postalcode  + '&country=' + country  + '&callback=getLocation&username=demo';
+    
+//       // Create a new script object
+//       aObj = new JSONscriptRequest(request);
+//       // Build the script tag
+//       aObj.buildScriptTag();
+//       // Execute (add) the script tag
+//       aObj.addScriptTag();
+
+// }
+// getWeatherForLocation();
 
 function showListings() {
     // var bounds = new google.maps.LatLngBounds();
